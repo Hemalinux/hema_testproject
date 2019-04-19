4d5c6c9c28fb613d6fd50b3c2246420ca5671a09
Hemauday <Hemauday@UdayaKumar.home>
2019-04-19 13:55:00 -0400

    first_testcase1

---
+++ Hema_git_project/src/git_package/sample_testforgit.java
@@ -0,0 +1,11 @@
+package git_package;
+
+public class sample_testforgit {
+
+	public static void main(String[] args) {
+		System.out.println("This is my selenium Training");
+		System.out.println("This is my Java Training");
+		
+	}
+
+}
